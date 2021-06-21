7f4165875ca252288c56176d239acc1345008286
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-06-21 10:38:55 +0200

    added tributes in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,9 @@ const Footer = () => {
   return (
     <div className="footer">
       <p>Created by Johan Löfgren and Marielle Ohlsson</p>
-      <p>Icon made by <a href=""></a></p>
+      <p>Icon made by <a href="https://www.cleanpng.com/png-passport-png-61817/">Sheelas</a></p>
       <p>Lottie-animation made by <a href="https://lottiefiles.com/nguyenguyen">Lottiefiles</a></p>
+      <p>Worldmap made by <a href="https://www.npmjs.com/package/react-svg-worldmap">Yanivam</a></p>
     </div>
   )
 }
