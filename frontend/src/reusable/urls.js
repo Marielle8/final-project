57f5aad17d09169a2eca369543951b421cdd572b
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-06-28 19:43:55 +0200

    added CI

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080'
-// const BASE_URL = 'https://mytraveljournalfinal.herokuapp.com/'
+// const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://my-travelguide.herokuapp.com/'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
